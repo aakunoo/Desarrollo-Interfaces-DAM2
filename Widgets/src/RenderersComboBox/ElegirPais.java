c65212fb9482fe66da5294e37c0e0449983b2dfe
Jerónimo Vicente <contact@jeronimovicente.com>
2024-12-13 14:17:28 +0100

    commit

---
+++ Widgets/src/RenderersComboBox/ElegirPais.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package RenderersComboBox;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JList;
+
+/**
+ *
+ * @author Manana
+ */
+public class ElegirPais extends JComboBox<Color> {
+    
+     public ElegirPais() {
+        this(new Color[] {Color.WHITE, Color.YELLOW, Color.ORANGE, Color.RED, Color.PINK, Color.GREEN, Color.CYAN, Color.BLUE, Color.MAGENTA, Color.BLACK});
+    }
+    
+public ElegirPais(Color[] colores) {
+        super(colores);
+        setRenderer(new DefaultListCellRenderer() {
+            @Override
+            public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) { 
+                JLabel l = (JLabel) super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+                l.setBackground((Color) value);
+                l.setText(" ");
+                return l;
+            }           
+        });
+        
+        addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setBackground((Color) getSelectedItem());
+            }
+        });
+    }
+}
